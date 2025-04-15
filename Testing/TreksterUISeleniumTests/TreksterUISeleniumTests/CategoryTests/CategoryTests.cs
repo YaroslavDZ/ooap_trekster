8c7435f59af763e632be08c20f0c7624994a335c
Yaroslav Dzysiuk <dzisyuk.yari@gmail.com>
2025-04-15 20:25:32 +0300

    add tests and gitignore

---
+++ Testing/TreksterUISeleniumTests/TreksterUISeleniumTests/CategoryTests/CategoryTests.cs
@@ -0,0 +1,61 @@
+﻿using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
+using OpenQA.Selenium;
+using SeleniumExtras.WaitHelpers;
+using WebDriverManager.DriverConfigs.Impl;
+using WebDriverManager;
+using TreksterUISeleniumTests.Helpers;
+
+namespace TreksterUISeleniumTests.CategoryTests
+{
+    public class CategoryTests
+    {
+        private const string BaseUrl = "https://localhost:7034";
+        private const string Email = "YourLogin";
+        private const string Password = "YourPassword";
+
+        [Fact(DisplayName = "Category Сreate Test")]
+        public void CreateEditDelete_Category_Lifecycle()
+        {
+            var createdCategoryName = "TestCat_" + Guid.NewGuid().ToString("N").Substring(0, 5);
+            var updatedCategoryName = createdCategoryName + "_Updated";
+
+            using var driver = InitDriver();
+            var wait = new WebDriverWait(driver, TimeSpan.FromSeconds(10));
+
+            Login(driver, wait);
+
+            // === CREATE ===
+            driver.Navigate().GoToUrl($"{BaseUrl}/Settings/Categories");
+            var addCategoryButton = wait.Until(ExpectedConditions.ElementToBeClickable(
+                By.CssSelector("a.btn-info[href='/Settings/Create']")));
+            ((IJavaScriptExecutor)driver).ExecuteScript("arguments[0].scrollIntoView(true);", addCategoryButton);
+            addCategoryButton.Click();
+
+            wait.Until(ExpectedConditions.ElementIsVisible(By.Name("Name")));
+            driver.FindElement(By.Name("Name")).SendKeys(createdCategoryName);
+            driver.FindElement(By.CssSelector("input[type='radio'][value='1']")).Click();
+            driver.FindElement(By.CssSelector("input[type='submit']")).Click();
+
+            wait.Until(ExpectedConditions.ElementExists(By.CssSelector("a.btn-info[href='/Settings/Create']")));
+            Assert.Contains(createdCategoryName, driver.FindElement(By.TagName("body")).Text);
+        }
+
+        private IWebDriver InitDriver()
+        {
+            new DriverManager().SetUpDriver(new ChromeConfig());
+            var driver = new ChromeDriver();
+            driver.Manage().Window.Maximize();
+            return driver;
+        }
+
+        private void Login(IWebDriver driver, WebDriverWait wait)
+        {
+            driver.Navigate().GoToUrl($"{BaseUrl}/User/Login");
+            driver.FindElement(By.Name("Email")).SendKeys(Email);
+            driver.FindElement(By.Name("Password")).SendKeys(Password);
+            driver.FindElement(By.CssSelector("button[type='submit']")).Click();
+            wait.Until(ExpectedConditions.UrlContains("/Home/Index"));
+        }
+    }
+}

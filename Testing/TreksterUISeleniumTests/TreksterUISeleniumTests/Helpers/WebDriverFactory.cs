d5abf1d9c87e2327f1f470586dc5db035a489e7c
dzisyuk.yari@gmail.com <dzisyuk.yari@gmail.com>
2025-04-16 01:31:21 +0300

    ignore sertificate

--- Testing/TreksterUISeleniumTests/TreksterUISeleniumTests/Helpers/WebDriverFactory.cs
+++ Testing/TreksterUISeleniumTests/TreksterUISeleniumTests/Helpers/WebDriverFactory.cs
@@ -7,7 +7,8 @@ namespace TreksterUISeleniumTests.Helpers
         public static ChromeDriver CreateDriver()
         {
             var options = new ChromeOptions();
-            options.AddArgument("--headless");
+            options.AddArgument("--ignore-certificate-errors");
+            options.AddArgument("--headless=new");
             options.AddArgument("--no-sandbox");
             options.AddArgument("--disable-dev-shm-usage");
             options.AddArgument($"--user-data-dir=/tmp/chrome-user-data-{Guid.NewGuid()}");
